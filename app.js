b4b7127594747a5707df1e5e5510edd8ad46cee1
pedrolima10 <146019647+pedrolima10@users.noreply.github.com>
2024-03-14 17:14:34 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const axios = require ('axios');
 const path = require ('path');
 const cors = require ('cors');
-const config = require('/config.json/');
+const config = require('./config.json');
 const apikey = config.apikey;
 
 const app = express();
@@ -12,30 +12,86 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const traducaoClima = {
-    "few clouds": "Poucas nuvens",
-    "scattered clouds": "Nuvens dispersas"
+
+const traducaoClima= {
+    'thunderstorm with light rain': 'trovoada com chuva fraca',
+    'thunderstorm with rain': 'trovoada com chuva',
+    'thunderstorm with heavy rain': 'trovoada com chuva forte',
+    'light thunderstorm': 'trovoada leve',
+    'thunderstorm': 'trovoada',
+    'heavy thunderstorm': 'forte tempestade',
+    'ragged thunderstorm': 'tempestade irregular',
+    'thunderstorm with light drizzle': 'trovoada com leve garoa',
+    'thunderstorm with drizzle': 'trovoada com garoa',
+    'thunderstorm with heavy drizzle': 'trovoada com forte garoa',
+    'light intensity drizzle': 'chuvisco de baixa intensidade',
+    'drizzle': 'chuvisco',
+    'heavy intensity drizzle': 'chuvisco de forte intensidade',
+    'light intensity drizzle rain': 'chuvisco de baixa intensidade com chuva',
+    'drizzle rain': 'chuvisco exponencial',
+    'heavy intensity drizzle rain': 'chuva forte com garoa',
+    'shower rain and drizzle': 'chuva e garoa',
+    'shower drizzle': 'chuvisco forte',
+    'light rain': 'chuva leve',
+    'clear sky': 'ceu limpo',
+    'few clouds': 'poucas nuvens',
+    'scattered clouds': 'nuvens dispersas',
+    'broken clouds': 'nuvens separadas',
+    'overcast clouds': 'muitas nuvens',  
+    'moderate rain': 'chuva moderada',
+    'heavy intensity rain': 'chuva de forte intensidade',
+    'very heavy rain': 'chuva muito forte',
+    'extreme rain': 'chuva extrema',
+    'freezing rain': 'chuva congelante',
+    'light intensity shower rain': 'banho de chuva de fraca intensidade',
+    'shower rain': 'banho de chuva',
+    'heavy intensity shower rain': 'banho de chuva de intensidade pesada',
+    'ragged shower rain': 'banho de chuva irregular',
+    'light snow': 'leve neve',
+    'snow': 'neve',
+    'heavy snow': 'neve pesada',
+    'sleet': 'granizo',
+    'light shower sleet': 'neve com granizo leve',
+    'shower sleet': 'neve com granizo',
+    'light rain and snow': 'chuva fraca e neve',
+    'rain and snow': 'chuva e chuva',
+    'light shower snow': 'chuva leve de neve',
+    'shower snow': 'chuva de neve',
+    'heavy shower snow': 'forte chuva de neve',
+    'mist': 'névoa',
+    'smoke': 'fumaça',
+    'haze': 'neblina',
+    'sand/dust whirls': 'redemoinhos de areia/poeira',
+    'fog': 'névoa',
+    'sand': 'areia',
+    'dust': 'poeira',
+    'volcanic ash': 'cinza vulcanica',
+    'squalls': 'ventania',
+    'tornado': 'tornado'
 }
 
-app.get('/climatempo/cidade', async (req, res) => {
-    const city = req.parametro.cidade;
+app.get('/climatempo/:cidade', async (req, res) => {
+
+    const city = req.params.cidade;
+   
 
     try{
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
+
             if(response.status === 200){
 
-                const clima = traducaoClima[response.data.weatcher[0].description] || response.data.weatcher[0].description;
+                const clima = traducaoClima[response.data.weather[0].description] || response.data.weather[0].description;
 
-                const weatcher = {
-                    temperatura: response.data.main.temp,
-                    umidade: response.data.main.humidity,
-                    velocidadeDoVento: response.data.wind.spead,
-                    clima: clima
+                const weatherData = {
+                    Temperatura: response.data.main.temp,
+                    Umidade: response.data.main.humidity,
+                    VelocidadeDoVento: response.data.wind.speed,
+                    Clima: clima
                 };
 
-                res.send(weatherData);
+                res.send(weatherData)   
         } else {
-                res.status(response.status).send({erro: 'Erro ao obter dados metereológicos'})
+                res.status(response.status).send({erro: 'Erro ao obter dados meteorológicos'})
         }
 
     } catch (error) {
---
+++ script.js
@@ -0,0 +1,120 @@
+function neblina() {
+    var body = document.querySelector('body'); // Seleciona a tag body
+    body.style.backgroundImage = 'url("./gif/neblina.gif")'; // Define a imagem de fundo
+    body.style.backgroundSize = "cover"; // Ajusta o tamanho da imagem para cobrir todo o fundo
+    body.style.backgroundRepeat = "no-repeat"; // Evita que a imagem se repita
+}
+function sol() {
+    var body = document.querySelector('body'); // Seleciona a tag body
+    body.style.backgroundImage = 'url("./gif/sun.gif")'; // Define a imagem de fundo
+    body.style.backgroundSize = "cover"; // Ajusta o tamanho da imagem para cobrir todo o fundo
+    body.style.backgroundRepeat = "no-repeat"; // Evita que a imagem se repita
+}
+
+function chuvaleve() {
+    var body = document.querySelector('body'); // Seleciona a tag body
+    body.style.backgroundImage = 'url("./gif/chuvaleve.gif")'; // Define a imagem de fundo
+    body.style.backgroundSize = "cover"; // Ajusta o tamanho da imagem para cobrir todo o fundo
+    body.style.backgroundRepeat = "no-repeat"; // Evita que a imagem se repita
+}
+
+function banhodechuva() {
+    var body = document.querySelector('body'); // Seleciona a tag body
+    body.style.backgroundImage = 'url("./gif/banhodechuva.gif")'; // Define a imagem de fundo
+    body.style.backgroundSize = "cover"; // Ajusta o tamanho da imagem para cobrir todo o fundo
+    body.style.backgroundRepeat = "no-repeat"; // Evita que a imagem se repita
+}
+
+function nublado() {
+    var body = document.querySelector('body'); // Seleciona a tag body
+    body.style.backgroundImage = 'url("./gif/nublado.gif")'; // Define a imagem de fundo
+    body.style.backgroundSize = "cover"; // Ajusta o tamanho da imagem para cobrir todo o fundo
+    body.style.backgroundRepeat = "no-repeat"; // Evita que a imagem se repita
+}
+
+function nuvensseparadas() {
+    var body = document.querySelector('body'); // Seleciona a tag body
+    body.style.backgroundImage = 'url("./gif/nuvensseparadas.gif")'; // Define a imagem de fundo
+    body.style.backgroundSize = "cover"; // Ajusta o tamanho da imagem para cobrir todo o fundo
+    body.style.backgroundRepeat = "no-repeat"; // Evita que a imagem se repita
+}
+
+function neveleve() {
+    var body = document.querySelector('body'); // Seleciona a tag body
+    body.style.backgroundImage = 'url("./gif/neve.gif")'; // Define a imagem de fundo
+    body.style.backgroundSize = "cover"; // Ajusta o tamanho da imagem para cobrir todo o fundo
+    body.style.backgroundRepeat = "no-repeat"; // Evita que a imagem se repita
+}
+
+function tempestade() {
+    var body = document.querySelector('body'); // Seleciona a tag body
+    body.style.backgroundImage = 'url("./gif/tempestade.gif")'; // Define a imagem de fundo
+    body.style.backgroundSize = "cover"; // Ajusta o tamanho da imagem para cobrir todo o fundo
+    body.style.backgroundRepeat = "no-repeat"; // Evita que a imagem se repita
+}
+
+function primeiraLetraMaiuscula(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+document.getElementById('formclima').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const city = document.getElementById('cityInput').value;
+
+    fetch(`http://localhost:3000/climatempo/${city}`)
+        .then(response => response.json())
+        .then(data => {
+
+            const tempoResult = document.getElementById('climaResult');
+
+            if (data.Temperatura) {
+
+                document.getElementById('city').textContent = primeiraLetraMaiuscula(city);
+                document.getElementById('temperatura').textContent = `${data.Temperatura}°C`;
+                document.getElementById('umidade').textContent = `${data.Umidade}%`;
+                document.getElementById('vento').textContent = `${data.VelocidadeDoVento}m/s`;
+                document.getElementById('descricao').textContent =`${data.Clima}`
+
+                document.getElementById('climaResult').style.display = 'flex';
+                document.getElementById('descricao1').style.display = 'flex';
+                document.getElementById('lupa').style.display = 'flex';
+                document.getElementById('formclima').style.display = 'none';  
+                
+
+                const temperaturaIcon = document.getElementById('temperaturaIcon');
+                const umidadeIcon = document.getElementById('umidadeIcon');
+                const ventoIcon = document.getElementById('ventoIcon');
+                const descricaoIcon = document.getElementById('descricaoIcon');
+
+                console.log(city);
+
+                cityIcon.src = "./icones/cidade.png";
+                temperaturaIcon.src = "./icones/termometro.png";
+                umidadeIcon.src = "./icones/umidade.png";
+                ventoIcon.src = "./icones/vento2.png";
+                descricaoIcon.src = "./icones/clima.png";
+
+
+                if (data.Clima == 'ceu limpo') {
+                    sol();
+                } else if (data.Clima == 'Nublado' || data.Clima == 'muitas nuvens') {
+                    nublado();
+                } else if (data.Clima == 'nuvens separadas' || data.Clima == 'nuvens dispersas' || data.Clima == 'poucas nuvens' || data.Clima == 'Nuvens nubladas'){
+                    nuvensseparadas()
+                } else if (data.Clima == 'leve chuva' || data.Clima == 'garoa leve' || data.Clima == 'chuvisco de baixa intensidade' || data.Clima == 'chuvisco' || data.Clima == 'chuva e garoa' || data.Clima == 'chuva leve') {
+                    chuvaleve();
+                } else if (data.Clima == 'neblina' || data.Clima == 'fumaça' || data.Clima == 'névoa' || data.Clima == 'poeira' || data.Clima == 'areia' || data.Clima == 'ventania') {
+                    neblina();
+                } else if (data.Clima == 'banho de chuva' || data.Clima == 'chuva media' || data.Clima == 'chuva moderada' || data.Clima == 'banho de chuva irregular' || data.Clima == 'chuvisco forte' || data.Clima == 'chuva forte com garoa' || data.Clima == 'chuvisco de alta intensidade') {
+                    banhodechuva();
+                } else if (data.Clima == 'tempestade' || data.Clima == 'forte tempestade' || data.Clima == 'tempestade irregular' || data.Clima == 'trovoada com chuva fraca' || data.Clima == 'trovoada com chuva' || data.Clima == 'trovoada com chuva forte' || data.Clima == 'trovoada leve' || data.Clima == 'trovoada' || data.Clima == 'trovoada com leve garoa' || data.Clima == 'trovoada com garoa' || data.Clima == 'trovoada com forte garoa') {
+                    tempestade();
+                }
+
+            } else {
+                tempoResult.innerHTML = "Erro ao obter dados metereologicos";
+            }
+        })
+        .catch((error) => console.error("Erro ao obter dados"));
+});
